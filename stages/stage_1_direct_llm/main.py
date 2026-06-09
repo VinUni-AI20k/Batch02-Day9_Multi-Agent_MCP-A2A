d18ccf8ec6ee28383161ad9e9c4490fc8657ce22
TrongVu249 <103042325+TrongVu249@users.noreply.github.com>
2026-06-09 14:21:55 +0700

    Run stage 1 succesfully

--- stages/stage_1_direct_llm/main.py
+++ stages/stage_1_direct_llm/main.py
@@ -19,7 +19,7 @@ from langchain_core.messages import HumanMessage, SystemMessage
 
 from common.llm import get_llm
 
-QUESTION = "What are the legal consequences if a company breaches a non-disclosure agreement?"
+QUESTION = "Is piracy legal in international waters?"
 
 
 async def main():
